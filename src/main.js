a5d588bd346e71b4bb6657d0f435599d667b2f05
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2018-02-25 17:07:10 -0300

    Initial structure based on vuetify template

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions,
+} from 'vuetify';
+import '../node_modules/vuetify/src/stylus/app.styl';
+import App from './App';
+import router from './router';
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions,
+  },
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107',
+  },
+});
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+});
